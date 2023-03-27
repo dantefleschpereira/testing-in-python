0c160b4f123f928481e8228472e1ae50ec534359
Dante <48799984+dantefleschpereira@users.noreply.github.com>
2023-03-27 14:18:39 -0300

    feat: inclusao test_soma_5_e_5_deve_retornar_10

--- calculadora/test_calculadora.py
+++ calculadora/test_calculadora.py
@@ -1,4 +1,10 @@
 import unittest
+from calculadora import soma
+
 
 class TestCalculadora(unittest.TestCase):
-    
+    def test_soma_5_e_5_deve_retornar_10(self):
+        self.assertEqual(soma(5, 5), 10)
+
+
+unittest.main(verbosity=2)
