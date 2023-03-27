4fca838a04f9e3db2af8ac005ceafb21938c2648
Dante <48799984+dantefleschpereira@users.noreply.github.com>
2023-03-27 14:21:51 -0300

    feat: test_2_negativo_e_6_deve_retornar_4

--- calculadora/test_calculadora.py
+++ calculadora/test_calculadora.py
@@ -6,5 +6,8 @@ class TestCalculadora(unittest.TestCase):
     def test_soma_5_e_5_deve_retornar_10(self):
         self.assertEqual(soma(5, 5), 10)
 
+    def test_2_negativo_e_6_deve_retornar_4(self):
+        self.assertEqual(soma(-2, 6), 4)
+
 
 unittest.main(verbosity=2)
