2c2d5c80ce58436d463a28a8609fac79137c5281
Dante <48799984+dantefleschpereira@users.noreply.github.com>
2023-03-28 09:23:40 -0300

    feat: ...deve_retornar_assertionerror

--- calculadora/test_calculadora.py
+++ calculadora/test_calculadora.py
@@ -9,5 +9,8 @@ class TestCalculadora(unittest.TestCase):
     def test_2_negativo_e_6_deve_retornar_4(self):
         self.assertEqual(soma(-2, 6), 4)
 
+    def test_soma_x_nao_e_int_ou_float_deve_retornar_assertionerror(self):
+        with self.assertRaises(AssertionError):
+            soma('11', 0)
 
 unittest.main(verbosity=2)
